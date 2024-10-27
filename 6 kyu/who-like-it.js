2b1231e54cc35fbf2dcf9ed8f74b637b29961ab4
Clem-Coding <clementine.blondeau@gmail.com>
2024-10-27 13:05:36 +0100

    Update who-like-it.js

--- 6 kyu/who-like-it.js
+++ 6 kyu/who-like-it.js
@@ -16,10 +16,9 @@
 // My Solution :
 
 function likes(names) {
-  switch (names.length) {
+  switch(names.length) {
     case 0:
-      F;
-      return "no one likes this";
+      return 'no one likes this';
     case 1:
       return `${names[0]} likes this`;
     case 2:
@@ -27,8 +26,6 @@ function likes(names) {
     case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     default:
-      return `${names[0]}, ${names[1]} and ${
-        names.length - 2
-      } others like this`;
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
 }
