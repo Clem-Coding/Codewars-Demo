aa1fa628b10faff90a13678c28bef3455a23a2cf
Clem-Coding <clementine.blondeau@gmail.com>
2024-11-17 15:19:31 +0100

    Kata of the day : split camelCase using regex

---
+++ 6 kyu/Break-camel-case.js
@@ -0,0 +1,15 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// My solution :
+
+function breakCamelCase(string) {
+  return string.replace(/[A-Z]/g, " " + "$&");
+}
+
+// For a more concise solution, instead of using " " + "$&", we could simply use " $&",
+//  or alternatively : (/([A-Z])/g," $1") , where $1 refers to the first captured group
