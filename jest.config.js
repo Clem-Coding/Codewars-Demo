d9834c79a4750325a15535112674c0fea706d89e
Clem-Coding <clementine.blondeau@gmail.com>
2025-09-19 10:41:59 +0200

    chore: update config and dependancies

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,25 @@
 export default {
-  transform: {},
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest",
+  },
   testMatch: [
     "<rootDir>/tests/5kyu/**/*.test.js",
     "<rootDir>/tests/6kyu/**/*.test.js",
+    "<rootDir>/tests/7kyu/**/*.test.ts",
+    "<rootDir>/tests/7kyu/**/*.test.js",
     "<rootDir>/tests/8kyu/**/*.test.js",
   ],
   moduleNameMapper: {
-    "^@5kyu/(.*)$": "<rootDir>/src/5kyu/$1.js",
-    "^@6kyu/(.*)$": "<rootDir>/src/6kyu/$1.js",
+    "^@5kyu/(.*)$": "<rootDir>/src/5kyu/$1",
+    "^@6kyu/(.*)$": "<rootDir>/src/6kyu/$1",
+    "^@7kyu/(.*)$": "<rootDir>/src/7kyu/$1",
   },
 };
