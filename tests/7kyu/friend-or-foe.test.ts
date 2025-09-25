9ab3c129fb7e76e1cd3428883bc0b3833c36f0cc
Clem-Coding <clementine.blondeau@gmail.com>
2025-09-25 09:35:36 +0200

    Kata of the day(7kyu): filtering on array of strings

---
+++ tests/7kyu/friend-or-foe.test.ts
@@ -0,0 +1,10 @@
+import { friend } from "@7kyu/friend-or-foe";
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    expect(friend(["Ryan", "Kieran", "Mark"])).toEqual(["Ryan", "Mark"]);
+    expect(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])).toEqual(["Ryan"]);
+    expect(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"])).toEqual(["Jimm", "Cari", "aret"]);
+    expect(friend(["Love", "Your", "Face", "1"])).toEqual(["Love", "Your", "Face"]);
+  });
+});
